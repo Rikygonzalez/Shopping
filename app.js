33d4cf96be984f8eebfaaf5c38df3d17acbe4dd4
Richard Gonzalez <rikygonzalez09@gmail.com>
2023-01-24 19:39:49 -0300

    Se agrego evento al menu mobile en JS al hacer click al icono de la hamburgesa

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+const burgerImage = document.querySelector(".icon-hamburger")
+const burgerMobileMenu = document.querySelector(".icon-hamburger-mobile")
+
+burgerImage.addEventListener("click", hiddenBurger)
+
+function hiddenBurger() {
+    const ocultarHamburgesa = burgerMobileMenu.classList.contains("inactive")
+
+    if (!ocultarHamburgesa) {
+        burgerMobileMenu.classList.add("inactive")
+    } else {
+        burgerMobileMenu.classList.remove("inactive")
+    }
+
+}
+
+
+
+
+
+
 const api = "https://api.escuelajs.co/api/v1/products/"
 
 window
