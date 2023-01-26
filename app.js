a0dd2dd9ef6b6371de17aadf51b501a13c5ad890
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-01-26 15:29:05 -0300

    Se realiza el codigo para la simulacion de inicio de sesion

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const burgerImage = document.querySelector(".icon-hamburger")
 const burgerMobileMenu = document.querySelector(".icon-hamburger-mobile")
 const buttonLogin = document.querySelector(".header-login")
 const buttonMobileLogin = document.querySelector(".button-login-mobile")
+const account = document.querySelector(".account")
+const formBtn = document.querySelector(".form-btn")
 
 burgerImage.addEventListener("click", hiddenBurger)
 buttonLogin.addEventListener("click", hiddenLogin)
+formBtn.addEventListener("click", hiddenForm)
 
 function hiddenBurger() {
     const ocultarHamburgesa = burgerMobileMenu.classList.contains("inactive")
@@ -26,6 +29,17 @@ function hiddenLogin() {
         buttonMobileLogin.classList.remove("inactive")
     }
     burgerMobileMenu.classList.add("inactive")
+}
+function hiddenForm(){
+    const ocultarForm = account.classList.contains("inactive")
+
+    if(ocultarForm){
+        account.classList.remove('inactive')
+        buttonLogin.classList.add("inactive")
+        buttonMobileLogin.classList.add("inactive")
+    } else {
+        burgerMobileMenu.classList.add("inactive")
+    }
 }
 
 
