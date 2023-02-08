6555005679b7313fc17e0dfb476f96b1910a42e1
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-02-08 12:25:50 -0300

    cambios con api funcionando

--- dom.js
+++ dom.js
@@ -48,63 +48,131 @@ renderProducts(productList);
 
 
 
-function categorias(arr) {
-    for (product of arr) {
-        const productCard = document.createElement('div');
-        productCard.classList.add('carousel-item');
+// function categorias(arr) {
+//     for (product of arr) {
+//         const productCard = document.createElement('div');
+//         productCard.classList.add('carousel-item');
     
-        // product= {name, price, image} -> product.image
-        const productImg = document.createElement('img');
-        productImg.setAttribute('src', product.categoria.images);
+//         const productImg = document.createElement('img');
+//         productImg.setAttribute('src', product.categoria.images);
     
-        const productInfo = document.createElement('div');
-        productInfo.classList.add('carousel-item__details');
+//         const productInfo = document.createElement('div');
+//         productInfo.classList.add('carousel-item__details');
     
-        const productInfoDiv = document.createElement('div');
+//         const productInfoDiv = document.createElement('div');
     
-        const productDescription = document.createElement('p');
-        productDescription.innerText = product.categoria.name;
+//         const productDescription = document.createElement('p');
+//         productDescription.innerText = product.categoria.name;
     
-        productInfoDiv.appendChild(productDescription);
+//         productInfoDiv.appendChild(productDescription);
     
-        const productInfoFigure = document.createElement('figure');
+//         const productInfoFigure = document.createElement('figure');
     
 
-        productInfo.appendChild(productInfoDiv);
-        productInfo.appendChild(productInfoFigure);
+//         productInfo.appendChild(productInfoDiv);
+//         productInfo.appendChild(productInfoFigure);
     
-        productCard.appendChild(productImg);
-        productCard.appendChild(productInfo);
+//         productCard.appendChild(productImg);
+//         productCard.appendChild(productInfo);
     
-        carouselCategorias.appendChild(productCard);
-    }
-};
+//         carouselCategorias.appendChild(productCard);
+//     }
+// };
 
-categorias(productList);
+// categorias(productList);
 
 
+const api = 'https://api.storerestapi.com'
 
-const api = 'https://api.storerestapi.com/products'
+window
+    .fetch(`${api}/products`)
+// procesar la respuesta y convertirla a JSON
+    .then((respuesta) => respuesta.json())
+// JSON --> DATA --> renderizar info al browser
+    .then((responseJson) => {
+        responseJson.data.forEach((item) => {
+            console.log(item);
 
-async function cargarProducts(){
-    const res = await fetch(api)
-    const data = await res.json()
-
-    console.log(data)
-
-    data.forEach(product => {
-        const productCard = document.createElement('div');
-        productCard.classList.add('carousel-item');
-    
-    });
+            const productCard = document.createElement('div');
+            productCard.classList.add('carousel-item');
+            
+            const productImg = document.createElement('img');
+            productImg.setAttribute('src', item.image);
         
-    if(res.status !== 200){
-        spanError.innerHTML = "Hubo un error: " + res.status + data.message;
-    }
-    };
+            const productInfo = document.createElement('div');
+            productInfo.classList.add('carousel-item__details');
+        
+            const productInfoDiv = document.createElement('div');
 
-cargarProducts()
+            const productTitle = document.createElement('p');
+            productTitle.innerText = item.title;
+            const productDescription = document.createElement('p');
+            productDescription.innerText = item.description;
+            const productPrice = document.createElement('p');
+            productPrice.innerText = '$' + item.price;
+        
+            productInfoDiv.appendChild(productTitle);
+            productInfoDiv.appendChild(productDescription);
+            productInfoDiv.appendChild(productPrice);
+        
+            const productInfoFigure = document.createElement('figure');
+        
+    
+            productInfo.appendChild(productInfoDiv);
+            productInfo.appendChild(productInfoFigure);
+        
+            productCard.appendChild(productImg);
+            productCard.appendChild(productInfo);
+    
+        
+            otro.appendChild(productCard)
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    })
 
+    window
+    .fetch(`${api}/categories`)
+// procesar la respuesta y convertirla a JSON
+    .then((respuesta) => respuesta.json())
+// JSON --> DATA --> renderizar info al browser
+    .then((responseJson) => {
+        responseJson.data.forEach((item) => {
+            console.log(item);
 
+            const productCard = document.createElement('div');
+            productCard.classList.add('carousel-item');
+            
+            const productImg = document.createElement('img');
+            productImg.setAttribute('src', item.image);
+        
+            const productInfo = document.createElement('div');
+            productInfo.classList.add('carousel-item__details');
+        
+            const productInfoDiv = document.createElement('div');
 
-
+            const productName = document.createElement('p');
+            productName.innerText = item.name;
+            const productDescription = document.createElement('p');
+            productDescription.innerText = item.products[0];
+        
+            productInfoDiv.appendChild(productName);
+            productInfoDiv.appendChild(productDescription);
+        
+            const productInfoFigure = document.createElement('figure');
+        
+    
+            productInfo.appendChild(productInfoDiv);
+            productInfo.appendChild(productInfoFigure);
+        
+            productCard.appendChild(productImg);
+            productCard.appendChild(productInfo);
+    
+        
+            carouselCategorias.appendChild(productCard)
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    })
