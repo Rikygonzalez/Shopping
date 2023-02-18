9bb072838a2974149137b41c268f9c7082bb4b6f
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-02-18 20:15:11 -0300

    uso de api dummyjson api y modificacion para la misma

--- dom.js
+++ dom.js
@@ -47,22 +47,22 @@ function renderProducts(arr) {
 renderProducts(productList);
 
 
-const api = 'https://api.storerestapi.com'
+const api = 'https://dummyjson.com/'
 
 window
-    .fetch("https://fakestoreapi.com/products")
+    .fetch('https://dummyjson.com/products')
 // procesar la respuesta y convertirla a JSON
     .then((respuesta) => respuesta.json())
 // JSON --> DATA --> renderizar info al browser
     .then((responseJson) => {
-        responseJson.forEach((item) => {
+        responseJson.products.forEach((item) => {
             console.log(item);
 
             const productCard = document.createElement('div');
             productCard.classList.add('carousel-item');
             
             const productImg = document.createElement('img');
-            productImg.setAttribute('src', item.image);
+            productImg.setAttribute('src', item.images[0]);
         
             const productInfo = document.createElement('div');
             productInfo.classList.add('carousel-item__details');
@@ -75,10 +75,13 @@ window
             productDescription.innerText = item.description;
             const productPrice = document.createElement('p');
             productPrice.innerText = '$' + item.price;
+            const productRating =document.createElement('p');
+            productRating.innerText = '🌟 ' + item.rating;
         
             productInfoDiv.appendChild(productTitle);
             productInfoDiv.appendChild(productDescription);
             productInfoDiv.appendChild(productPrice);
+            productInfoDiv.appendChild(productRating);
         
             const productInfoFigure = document.createElement('figure');
         
@@ -96,69 +99,22 @@ window
     .catch(err => {
         console.log(err);
     })
-    
+
     window
-    .fetch(`${api}/products?limit=10&page=2`)
+    .fetch('https://dummyjson.com/products/categories')
 // procesar la respuesta y convertirla a JSON
     .then((respuesta) => respuesta.json())
 // JSON --> DATA --> renderizar info al browser
     .then((responseJson) => {
-        responseJson.data.forEach((item) => {
+        console.log(responseJson);
+        responseJson.forEach((item) => {
             console.log(item);
 
             const productCard = document.createElement('div');
             productCard.classList.add('carousel-item');
             
             const productImg = document.createElement('img');
-            productImg.setAttribute('src', item.image);
-        
-            const productInfo = document.createElement('div');
-            productInfo.classList.add('carousel-item__details');
-        
-            const productInfoDiv = document.createElement('div');
-
-            const productTitle = document.createElement('p');
-            productTitle.innerText = item.title;
-            const productDescription = document.createElement('p');
-            productDescription.innerText = item.description;
-            const productPrice = document.createElement('p');
-            productPrice.innerText = '$' + item.price;
-        
-            productInfoDiv.appendChild(productTitle);
-            productInfoDiv.appendChild(productDescription);
-            productInfoDiv.appendChild(productPrice);
-        
-            const productInfoFigure = document.createElement('figure');
-        
-    
-            productInfo.appendChild(productInfoDiv);
-            productInfo.appendChild(productInfoFigure);
-        
-            productCard.appendChild(productImg);
-            productCard.appendChild(productInfo);
-    
-        
-            otros.appendChild(productCard)
-        });
-    })
-    .catch(err => {
-        console.log(err);
-    })
-
-    window
-    .fetch(`${api}/categories`)
-// procesar la respuesta y convertirla a JSON
-    .then((respuesta) => respuesta.json())
-// JSON --> DATA --> renderizar info al browser
-    .then((responseJson) => {
-        responseJson.data.forEach((item) => {
-            console.log(item);
-
-            const productCard = document.createElement('div');
-            productCard.classList.add('carousel-item');
-            
-            const productImg = document.createElement('img');
-            productImg.setAttribute('src', item.image);
+            productImg.setAttribute('src', './Image/icon-instagram.svg');
         
             const productInfo = document.createElement('div');
             productInfo.classList.add('carousel-item__details');
@@ -166,12 +122,9 @@ window
             const productInfoDiv = document.createElement('div');
 
             const productName = document.createElement('p');
-            productName.innerText = item.name;
-            const productDescription = document.createElement('p');
-            productDescription.innerText = item.products[1];
+            productName.innerText = item.toUpperCase() ;
         
             productInfoDiv.appendChild(productName);
-            productInfoDiv.appendChild(productDescription);
         
             const productInfoFigure = document.createElement('figure');
         
